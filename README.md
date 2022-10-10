24e6626d536c2af736e6e134e291cc0d43527359
Clark Rasmussen <cjr@clarkrasmussen.com>
2022-10-10 12:36:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# TrelloUpcomingNotification
+
+Simple class for checking one or more Trello lists for due or overdue cards, then sending a message about them via a Slack webhook. Not affiliated with Trello or Slack.
+
+
+## Usage
+
+```php
+use cjrasmussen\SlackApi\SlackApi;
+use cjrasmussen\TrelloApi\TrelloApi;
+use cjrasmussen\TrelloUpcomingNotification\TrelloUpcomingNotification;
+
+$trello_lists = [
+	'123456789012345678901234', // In Progress
+	'098765432109876543210987', // Not Started
+];
+$trello_ignore_label = 'asdfghjklpoiuytrewqzxcvb';
+
+$trelloNotification = new TrelloUpcomingNotification($trello_lists, $trello_ignore_label);
+
+$trello = new TrelloApi($key, $token);
+$trelloNotification->executeCheck($trello);
+if ($trelloNotification->areNotificationsAvailable()) {
+	$slack = new SlackApi($slack_webhook_url);
+	$trelloNotification->sendSlackNotification($slack);
+}
+```
+
+## Installation
+
+Simply add a dependency on cjrasmussen/trello-upcoming-notification to your composer.json file if you use [Composer](https://getcomposer.org/) to manage the dependencies of your project:
+
+```sh
+composer require cjrasmussen/trello-upcoming-notification
+```
+
+TrelloUpcomingNotification has dependencies on `cjrasmussen\SlackApi` and `cjrasmussen\TrelloApi` so these will be installed as well.
+
+Although it's recommended to use Composer, you can actually include the file(s) any way you want.
+
+
+## License
+
+TrelloUpcomingNotification is [MIT](http://opensource.org/licenses/MIT) licensed.
---
+++ src/TrelloUpcomingNotification.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace cjrasmussen\TrelloUpcomingNotification;
+
+use cjrasmussen\SlackApi\SlackApi;
+use cjrasmussen\TrelloApi\TrelloApi;
+
+class TrelloUpcomingNotification
+{
+	private const NOTIFICATION_TYPE_TODAY = 'today';
+	private const NOTIFICATION_TYPE_OVERDUE = 'overdue';
+
+	private const NOTIFICATION_TYPE_NAMES = [
+		self::NOTIFICATION_TYPE_TODAY => 'Due Today',
+		self::NOTIFICATION_TYPE_OVERDUE => 'Overdue',
+	];
+
+	private array $checkLists;
+	private array $ignoreLabels;
+	private string $checkDate;
+	private array $notification = [
+		self::NOTIFICATION_TYPE_TODAY => [],
+		self::NOTIFICATION_TYPE_OVERDUE => [],
+	];
+
+	/**
+	 * @param array $checkLists
+	 * @param array|string|null $ignoreLabels
+	 * @param string|null $checkDate
+	 */
+	public function __construct(array $checkLists, $ignoreLabels = null, ?string $checkDate = null)
+	{
+		$this->checkLists = $checkLists;
+
+		if (is_array($ignoreLabels)) {
+			$this->ignoreLabels = $ignoreLabels;
+		} elseif ($ignoreLabels) {
+			$this->ignoreLabels = [$ignoreLabels];
+		} else {
+			$this->ignoreLabels = [];
+		}
+
+		$this->checkDate = ($checkDate) ? date('Y-m-d', strtotime($checkDate)) : date('Y-m-d');
+	}
+
+	public function executeCheck(TrelloApi $trelloApi): void
+	{
+		foreach ($this->checkLists AS $list_id) {
+			// GET THE CARD DATA FOR THE LIST
+			$data = $trelloApi->request('GET', ('/1/lists/' . $list_id . '/cards'));
+
+			foreach ($data AS $card) {
+				if ((!$card->due) || (count(array_intersect($this->ignoreLabels, $card->idLabels))) > 0) {
+					continue;
+				}
+
+				$due_date = date('Y-m-d', strtotime($card->due));
+				if ($due_date === $this->checkDate) {
+					$this->notification[self::NOTIFICATION_TYPE_TODAY][] = [
+						'title' => $card->name,
+						'url' => $card->url,
+					];
+				} elseif ($due_date < $this->checkDate) {
+					$this->notification[self::NOTIFICATION_TYPE_OVERDUE][] = [
+						'title' => $card->name,
+						'url' => $card->url,
+						'due_date' => date('n/j/Y', strtotime($card->due)),
+					];
+				}
+			}
+		}
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function areNotificationsAvailable(): bool
+	{
+		return ((count($this->notification[self::NOTIFICATION_TYPE_TODAY]) > 0) || ($this->notification[self::NOTIFICATION_TYPE_OVERDUE] > 0));
+	}
+
+	/**
+	 * @param SlackApi $slackApi
+	 * @param string|null $channel
+	 * @return bool|null
+	 */
+	public function sendSlackNotification(SlackApi $slackApi, ?string $channel = null): ?bool
+	{
+		if (!$this->areNotificationsAvailable()) {
+			return null;
+		}
+
+		$blocks = [
+			(object)[
+				'type' => 'header',
+				'text' => (object)[
+					'type' => 'plain_text',
+					'text' => 'Upcoming Due Trello Tasks',
+				],
+			],
+			(object)[
+				'type' => 'divider',
+			],
+		];
+
+		foreach ($this->notification AS $notification_type => $notification_items) {
+			$item_count = count($notification_items);
+
+			if ($item_count) {
+				$item_count_word = 'item' . (($item_count === 1) ? '' : 's');
+
+				$blocks[] = (object)[
+					'type' => 'header',
+					'text' => (object)[
+						'type' => 'plain_text',
+						'text' => self::NOTIFICATION_TYPE_NAMES[$notification_type] . ' (' . $item_count . ' ' . $item_count_word . ')',
+					],
+				];
+
+				foreach ($notification_items AS $item) {
+					$blocks[] = (object)[
+						'type' => 'section',
+						'text' => (object)[
+							'type' => 'mrkdwn',
+							'text' => '<' . $item['url'] . '|' . $item['title'] . '>',
+						],
+					];
+					if ($item['due_date']) {
+						$blocks[] = (object)[
+							'type' => 'context',
+							'elements' => [
+								(object)[
+									'type' => 'mrkdwn',
+									'text' => '*Due Date:* ' . $item['due_date'],
+								],
+							],
+						];
+					}
+				}
+			}
+		}
+
+		$msg = [
+			'blocks' => $blocks,
+		];
+
+		if ($channel) {
+			$msg['channel'] = $channel;
+		}
+
+		return $slackApi->sendMessage($msg);
+	}
+}
